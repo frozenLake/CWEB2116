24456be670a27e24b3ebff5c5308761a5c418dd8
frozenLake <frozenLake123@gmail.com>
2023-11-15 10:00:16 -0600

    HW04 Added

---
+++ HW_04/src/abstractTest1Pack/XYZ.java
@@ -0,0 +1,11 @@
+package abstractTest1Pack;
+public class XYZ extends ABC{
+	String DAY(String greeting) {
+		return greeting + " THURSDAY";
+	}
+
+	@Override
+	String DAY() {
+		return null;
+	}
+}
